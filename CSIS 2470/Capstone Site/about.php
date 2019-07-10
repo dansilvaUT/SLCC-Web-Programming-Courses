8849fefe87c5f0fdc4d17003a707660289f0bf50
dan <dan13silv@gmail.com>
2019-07-09 18:20:43 -0600

    Added about file.

---
+++ CSIS 2470/Capstone Site/about.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'Header/header.php';
+?>
+
+<section class="page-section about-heading">
+      <div class="container">
+
+           <!--Image Carousel.  Just add the source!-->
+                <div class="fotorama" data-nav="thumbs" data-loop="true" data-keyboard="true" style="margin-bottom: 10%;">
+                    <img src="Images/about_images/about.jpg" alt="">
+                    <img src="Images/about_images/gal02.jpg">
+                    <img src="Images/about_images/gal03.jpg">
+                    <img src="Images/about_images/gal04.jpg">
+                    <img src="Images/about_images/gal05.jpg">
+                    <img src="Images/about_images/gal06.jpg">
+                    <img src="Images/about_images/gal07.jpg" alt="">
+
+
+
+                </div>
+
+
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">Are you ready to</span>
+                  <span class="section-heading-lower">Level Up!</span>
+                </h2>
+                <p> Do you have what it takes to seek adventure, fame, and fortune? Come join us in a land  rich with adventure, mystery and intrigue, magic and necromancy, wealth, power, and more. Whether you want to be a heroic warrior, dashing rogue, a craftsman, a travelling bard or poet, famed healer, or a mighty wizard, we have created a world that's waiting just for you. Alliance LARP Utah is a live action role-playing game set in a medieval/fantasy world where you take on a persona of your choosing and interact with other players and a cast of NPCs both benevolent and malign. There are no scripts as the outcomes to many plotlines are decided by your actions. Put down the dice and paper, log off, grab your gear and come join Alliance LARP Utah on a new quest. </p>
+                <p class="mb-0">Alliance Live Action Role-Playing Utah is a group committed to bringing action-packed weekend adventures to the LARP Community. Through quality writing and plotlines that establish setting, various dynamic NPC characters, and conflicts great and small, we are dedicated to bringing you the best live gaming experience.  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php
+include_once 'Footer/footer.php';
+?>
