4de25ba1f8abb8277593ceec37a9e625a6440970
dan <dan13silv@gmail.com>
2019-07-09 18:21:21 -0600

    Added footer partial.

---
+++ CSIS 2470/Capstone Site/Footer/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; 2018 Alliance Larp Utah - All Rights Reserved.</p>
+      </div>
+    </footer>
+
+
+
+  </body>
+
+</html>
